b31019139c107626e7c86d9e9825e3eaa87d2105
wuhao4u <wuhao4u@gmail.com>
2016-07-12 15:03:44 +0800

    finished Q1, Q 289 (there's gotta be a better way for this one)

--- leetCode/JavaSolutions/src/net/wuhao4u/Main.java
+++ leetCode/JavaSolutions/src/net/wuhao4u/Main.java
@@ -6,17 +6,41 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        Solution26 sol = new Solution26();
-        int[] test1 = {1,1,2};
-        System.out.println(sol.removeDuplicates(test1));
-        System.out.println(Arrays.toString(test1));
+        Solution289 sol = new Solution289();
 
-        int[] test2 = {1,1,1,2,3,3,3,0,0,0,0,0};
-        System.out.println(sol.removeDuplicates(test2));
-        System.out.println(Arrays.toString(test2));
+        int[][] test = new int[2][2];
+        test[0][0] = 0;
+        test[0][1] = 1;
+        test[1][0] = 1;
+        test[1][1] = 1;
+//        test[0][2] = 1;
+//        test[0][3] = 0;
+//
+//        test[1][0] = 1;
+//        test[1][1] = 1;
+//        test[1][2] = 0;
+//        test[1][3] = 1;
+//
+//        test[2][0] = 1;
+//        test[2][1] = 0;
+//        test[2][2] = 1;
+//        test[2][3] = 1;
 
-        int[] test3 = {1,2};
-        System.out.println(sol.removeDuplicates(test3));
-        System.out.println(Arrays.toString(test3));
+
+        //        Random ran = new Random();
+//        int val = 0;
+//
+//        for(int r = 0; r<3; r++) {
+//            for(int c = 0; c<4; c++) {
+//                val = ran.nextInt(100);
+//                test[r][c] = val < 50 ? 0 : 1;
+//            }
+//        }
+
+        sol.gameOfLife(test);
+
+        for(int r = 0; r < test.length; ++r) {
+            System.out.println(Arrays.toString(test[r]));
+        }
     }
 }
